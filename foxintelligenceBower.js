18a30d503524ead86d16528179fb8d3143be88a9
langrand <ludo.langrand@gmail.com>
2017-09-25 15:25:49 +0200

    create obj

--- foxintelligenceBower.js
+++ foxintelligenceBower.js
@@ -1,6 +1,31 @@
-(function($) {
-  $.fn.fiHtmlToJson = function(file){
-      console.log('jquery object oki')
-    return this;
-  };
+(function ($) {
+
+    var cleanHtml = function (string) {
+        let str = string.replace(/\\r\\n/g, '');
+        str = str.replace(/\\"/g, '"');
+        return str;
+    }
+    var htmlToJson = function (str) {
+        let jqObj = $(str);
+        var obj = {};
+        console.log('test = ', jqObj.find('table.passengers'));
+        return obj;
+    }
+    $.fn.fiHtmlToJson = function (file) {
+
+        if (file) {
+            var reader = new FileReader();
+            reader.readAsText(file, "UTF-8");
+            reader.onload = function (evt) {
+                var str = evt.target.result;
+                str = cleanHtml(str);
+                console.log('str 2 = ', str);
+                var obj = htmlToJson(str);
+                return obj;
+            }
+            reader.onerror = function (evt) {
+                console.log('error');
+            }
+        }
+    };
 })(jQuery);
